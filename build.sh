b230dfe7dfd60e20df26c94cc5c98d07837f4214
Dat Nguyen <datnguyen@chotot.vn>
2022-02-26 23:16:47 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -108,7 +108,7 @@ function commitAndPush {
 
 function installProtoc {
   PROTOC_ZIP=protoc-3.19.4-osx-x86_64.zip
-  curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
+  curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.19.4/$PROTOC_ZIP
   sudo unzip -o $PROTOC_ZIP -d /usr/local bin/protoc
   sudo unzip -o $PROTOC_ZIP -d /usr/local 'include/*'
   rm -f $PROTOC_ZIP
